c326c1fdce52fad1f588284ff8669a0348ee99a8
Suri Ho <surihsuri0@gmail.com>
2025-11-21 15:38:50 -0500

    Create new test class for add favorite use case.

---
+++ src/Tests/AddFavoriteInteractorTest.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import Classes.add_favorite.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AddFavoriteInteractorTest {
+
+    @Test
+    public void successTest() {
+        AddFavoriteInputData inputData = new AddFavoriteInputData("Cheetah");
+    }
+}
