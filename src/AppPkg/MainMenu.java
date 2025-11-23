feebbec9026c63ca244150934096660d18bd4a80
bsu <cheb.su@mil.utoronto.ca>
2025-11-23 12:04:34 -0500

    fixed some problems

--- src/AppPkg/MainMenu.java
+++ src/AppPkg/MainMenu.java
@@ -273,8 +273,9 @@ public class  MainMenu extends javax.swing.JFrame
     }//GEN-LAST:event_btnFavoritesActionPerformed
 
     private void updateLabelStyle(){
-        styleUpdater.updateAll(this);
-        lblGreeting1.setFont(new Font(
+        textSettingOutput.updateAll(this);
+        lblGreeting1.setFont(
+                new Font(
                 config.getStyleName(),
                 0,
                 36
